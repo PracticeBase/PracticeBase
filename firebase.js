495a112d6075ff97379db9089df8f3dc6a5b32df
Duckers <198241505+TheRealDuckers@users.noreply.github.com>
2026-02-04 16:42:29 +0000

    fixed issues

--- firebase.js
+++ firebase.js
@@ -9,26 +9,31 @@ import {
 import {
   getFirestore,
   collection,
+  doc,
+  setDoc,
+  getDoc,
   addDoc,
   onSnapshot,
-  serverTimestamp
+  serverTimestamp,
+  deleteDoc,
+  query,
+  orderBy
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAecPf2aXR0j1buuDmz9MVrS8aYINs1eCU",
-  authDomain: "grease-practicebase.firebaseapp.com",
-  projectId: "grease-practicebase",
-  storageBucket: "grease-practicebase.firebasestorage.app",
-  messagingSenderId: "696484090016",
-  appId: "1:696484090016:web:4d7bfe336d842ed946b9c5",
-  measurementId: "G-4C78RPXQ99"
-};
+    apiKey: "AIzaSyAecPf2aXR0j1buuDmz9MVrS8aYINs1eCU",
+    authDomain: "grease-practicebase.firebaseapp.com",
+    projectId: "grease-practicebase",
+    storageBucket: "grease-practicebase.firebasestorage.app",
+    messagingSenderId: "696484090016",
+    appId: "1:696484090016:web:4d7bfe336d842ed946b9c5",
+    measurementId: "G-4C78RPXQ99"
+  };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-const ADMIN_EMAILS = ["admin@example.com"];
 
 export {
   auth,
@@ -37,8 +42,13 @@ export {
   onAuthStateChanged,
   signOut,
   collection,
+  doc,
+  setDoc,
+  getDoc,
   addDoc,
   onSnapshot,
   serverTimestamp,
-  ADMIN_EMAILS
+  deleteDoc,
+  query,
+  orderBy
 };
